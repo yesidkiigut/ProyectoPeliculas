f47e6856bae0e64027a9f5140187b22da4a57c62
Yesid Quintero Gutierrez <yesidkiigut@gmail.com>
2023-04-10 19:01:10 -0500

    se agrega un ejemplo de footer para completar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import {MisRutas} from "../src/routers/routes";
+import  '../src/components/footer.css'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,8 +13,23 @@ function App() {
       <h1 className='title'>🎬..Peliculiculas Premium..🎬</h1>
       </header>
       <MisRutas/>
+      <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="item">
+                        <ul>
+                            <li><a href="#">Diseñado por: Yesid Quintero Gutierrez</a></li>
+                            <li><a href="#">React.js Desarrollador Frontend</a></li>
+                        </ul>
+                    </div>
+                <p class="copyright">Copyring 2023</p>
+            </div>
+            </div>
+        </footer>
     </div>
-  )
+    </div>        
+  )  
 }
 
 export default App
